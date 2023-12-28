824438bdc8b4367b97325da0cbaffdc91ffb163a
Ayhan Metin <108268012+ayhanmetin@users.noreply.github.com>
2023-12-28 21:19:32 +0100

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,22 +13,11 @@ function Home() {
       >
         <header className="name">
           <p className="text-start mb-0 pt-2 mt-5 border-start ps-3 mb-5 p-1 border-5 border-primary">
-            Web Applications enthusiast and dynamic engineer skilled in react
-            development, <br /> quality assurance, and agile practices.
+            <p className='ms-3'>Web Applications enthusiast and dynamic engineer skilled in react
+            development, quality assurance, and agile practices.</p>
           </p>
+          <div className="mt-2 mb-5 text-with-gradient">Skills</div>{' '}
         </header>
-        <div className="mt-2 mb-5 text-with-gradient">Skills</div>{' '}
-        <p>
-          A passionate frontend developer from Germany. This is a long paragraph
-          written to show how the
-        </p>
-        <p className="text-body-secondary">
-          Latest writing{' '}
-          <a href="#" className="text-reset">
-            reset link
-          </a>
-          .
-        </p>
         <Skills />
       </div>
     </>
