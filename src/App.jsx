ae5c213838de2892bd8c571be64f1503f275a89b
Ayhan Metin <108268012+ayhanmetin@users.noreply.github.com>
2023-12-28 17:47:13 +0100

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav from './components/Nav';
-import SiteRoutes from './SiteRoutes';
+import Home from './components/Home';
+import Writing from './components/Writing';
+import TestingHub from './components/Testinghub';
+import Portfolio from './components/Portfolio';
+import { Route, Routes } from 'react-router-dom';
+import PageNotFound from './pages/PageNotFound';
+
 function App() {
   return (
     <>
       <div
         style={{ margin: '0', padding: '0', marginBottom: '10px' }}
-        className="container"
+        className='container'
       >
-        <div className="row m-0">
-          <div className="col-2 pl-0 d-none d-xl-block m-0 p-0">
+        <div className='row m-0'>
+          <div className='col-2 pl-0 d-none d-xl-block m-0 p-0'>
             <Nav />
           </div>
-          <div className="col-12 col-xl-8 align-self-right">
-            <SiteRoutes />
+          <div className='col-12 col-xl-8 align-self-right'>
+            <Routes>
+              <Route path='/' element={<Home />} />
+              <Route path='/portfolio' element={<Portfolio />} />
+              <Route path='/testinghub' element={<TestingHub />} />
+              <Route path='/writing' element={<Writing />} />
+
+              <Route path='*' element={<PageNotFound />} />
+            </Routes>
           </div>
         </div>
       </div>
