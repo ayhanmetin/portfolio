c3cc6571a813784356cbbabe0e76236f92e8cb63
Ayhan Metin <108268012+ayhanmetin@users.noreply.github.com>
2023-12-28 23:12:03 +0100

    initial commit

--- src/projects.js
+++ src/projects.js
@@ -1,4 +1,4 @@
-import calcImage from './images/calc.png';
+import portfolioold from './images/portfolioold.png';
 import ecommerceImage from './images/e-commerce.png';
 import moleImage from './images/mole-cover.png';
 import calculatorIOS from './images/calculator.png';
@@ -20,9 +20,37 @@ const projects = [
   },
   {
     name: 'Portfolio Page',
-    imageUrl: calculatorIOS,
+    imageUrl: portfolioold,
     technologies: ['JS', 'HTML', 'CSS'],
-    githubUrl: 'https://github.com/project4',
+    githubUrl: 'https://github.com/ayhanmetin/portfolioJSpage',
+    liveDemoUrl: 'https://portfoliopage-rho.vercel.app/',
+  },
+  {
+    name: 'Calculator',
+    imageUrl: calculatorIOS,
+    technologies: ['JS', 'CSS', 'HTML'],
+    githubUrl: 'https://github.com/ayhanmetin/calculator',
+    liveDemoUrl: 'https://calculator-mu-smoky-14.vercel.app/',
+  },
+  {
+    name: 'Feed-A-Star-Mole',
+    imageUrl: moleImage,
+    technologies: ['JS', 'HTML', 'CSS'],
+    githubUrl: 'https://feed-a-mole-red.vercel.app/',
+    liveDemoUrl: 'https://github.com/ayhanmetin/feed-a-mole',
+  },
+  {
+    name: 'E-Commerce Page',
+    imageUrl: ecommerceImage,
+    technologies: ['React', 'Bootstrap', 'JS'],
+    githubUrl: 'https://github.com/ayhanmetin/ayhando-ecommerce',
+    liveDemoUrl: 'https://ayhando-ecommerce.vercel.app/',
+  },
+  {
+    name: 'Portfolio Page',
+    imageUrl: portfolioold,
+    technologies: ['JS', 'HTML', 'CSS'],
+    githubUrl: 'https://github.com/ayhanmetin/portfolioJSpage',
     liveDemoUrl: 'https://portfoliopage-rho.vercel.app/',
   },
   {
