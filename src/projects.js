d1aca799864d0a002c176702afa4f5043c8846d7
Ayhan Metin <108268012+ayhanmetin@users.noreply.github.com>
2023-12-29 23:42:30 +0100

    Update Testing.jsx

--- src/projects.js
+++ src/projects.js
@@ -32,34 +32,6 @@ const projects = [
     githubUrl: 'https://github.com/ayhanmetin/calculator',
     liveDemoUrl: 'https://calculator-mu-smoky-14.vercel.app/',
   },
-  {
-    name: 'Feed-A-Star-Mole',
-    imageUrl: moleImage,
-    technologies: ['JS', 'HTML', 'CSS'],
-    githubUrl: 'https://github.com/ayhanmetin/feed-a-mole',
-    liveDemoUrl: 'https://feed-a-mole-red.vercel.app/',
-  },
-  {
-    name: 'E-Commerce Page',
-    imageUrl: ecommerceImage,
-    technologies: ['React', 'Bootstrap', 'JS'],
-    githubUrl: 'https://github.com/ayhanmetin/ayhando-ecommerce',
-    liveDemoUrl: 'https://ayhando-ecommerce.vercel.app/',
-  },
-  {
-    name: 'Portfolio Page',
-    imageUrl: portfolioold,
-    technologies: ['JS', 'HTML', 'CSS'],
-    githubUrl: 'https://github.com/ayhanmetin/portfolioJSpage',
-    liveDemoUrl: 'https://portfoliopage-rho.vercel.app/',
-  },
-  {
-    name: 'Calculator',
-    imageUrl: calculatorIOS,
-    technologies: ['JS', 'CSS', 'HTML'],
-    githubUrl: 'https://github.com/ayhanmetin/calculator',
-    liveDemoUrl: 'https://calculator-mu-smoky-14.vercel.app/',
-  },
 ];
 
 export default projects;
