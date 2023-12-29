404de08cac3c529ce0676cb659ef9fc49937c692
Ayhan Metin <108268012+ayhanmetin@users.noreply.github.com>
2023-12-29 23:57:34 +0100

    initial commit

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -3,92 +3,68 @@ import React from 'react';
 export default function Skills() {
   return (
     <>
-      <div className='container mt-5 m-0 p-0 fs-5 text'>
-        <div className='mt-2 d-block fs-5 text fw-semibold text-decoration-underline'>
-          Technologies
+      <div className='container mt-5'>
+        <div className='text-start mb-0 pt-2 border-start ps-3 p-1 border-5 border-primary'>
+          <h2 className='fs-5 fw-semibold text-decoration-underline '>
+            Technologies
+          </h2>
         </div>
-        <div className='m-0 p-0 d-flex gap-2 py-4'>
-          <span className='badge p-2 text-bg-dark border border-primary'>
-            React
-          </span>
-          <span className='badge p-2 text-bg-dark border border-primary'>
+        <div className='d-flex flex-wrap gap-2 py-4 fs-4 text'>
+          <span className='badge bg-dark border border-primary'>React</span>
+          <span className='badge bg-dark border border-primary'>
             Javascript
           </span>
-          <span className='badge p-2 text-bg-dark border border-primary'>
+          <span className='badge bg-dark border border-primary'>
             Typescript
           </span>
-          <span className='badge p-2 text-bg-dark border border-primary'>
-            Git
-          </span>
-          <span className='badge p-2 text-bg-dark border border-primary'>
-            Python
-          </span>
-          <span className='badge p-2 text-bg-dark border border-primary'>
-            SQL
-          </span>
-          <span className='badge p-2 text-bg-dark border border-primary'>
-            Java
-          </span>
-          <span className='badge p-2 text-bg-dark border border-primary'>
-            Bootstrap
-          </span>
-          <span className='badge p-2 text-bg-dark border border-primary'>
-            Dockers
-          </span>
-          <span className='badge p-2 text-bg-dark border border-primary'>
-            Figma
-          </span>
+          <span className='badge bg-dark border border-primary'>Git</span>
+          <span className='badge bg-dark border border-primary'>Python</span>
+          <span className='badge bg-dark border border-primary'>SQL</span>
+          <span className='badge bg-dark border border-primary'>Java</span>
+          <span className='badge bg-dark border border-primary'>Bootstrap</span>
+          <span className='badge bg-dark border border-primary'>Dockers</span>
+          <span className='badge bg-dark border border-primary'>Figma</span>
         </div>
       </div>
-      <div className='container mt-3 m-0 p-0 fs-5 text'>
-        <div className='mt-2 d-block fs-5 text fw-semibold text-decoration-underline'>
-          Testing Tools
+
+      <div className='container mt-3 fs-4 text'>
+        <div className='text-start mb-0 pt-2 border-start ps-3 p-1 border-5 border-primary '>
+          <h2 className='fs-5 fw-semibold text-decoration-underline'>
+            Testing Tools
+          </h2>
         </div>
-        <div className='m-0 p-0 d-flex gap-2 py-4'>
-          <span className='badge p-2 text-bg-dark border border-primary'>
-            Cypress
-          </span>
-          <span className='badge p-2 text-bg-dark border border-primary'>
-            Selenium
-          </span>
-          <span className='badge p-2 text-bg-dark border border-primary'>
+        <div className='d-flex flex-wrap gap-2 py-4'>
+          <span className='badge bg-dark border border-primary'>Cypress</span>
+          <span className='badge bg-dark border border-primary'>Selenium</span>
+          <span className='badge bg-dark border border-primary'>
             Playwright
           </span>
-          <span className='badge p-2 text-bg-dark border border-primary'>
-            Jest
-          </span>
-          <span className='badge p-2 text-bg-dark border border-primary'>
-            Pytest
-          </span>
-          <span className='badge p-2 text-bg-dark border border-primary'>
-            Puppeteer
-          </span>
-          <span className='badge p-2 text-bg-dark border border-primary'>
-            Postman
-          </span>
-          <span className='badge p-2 text-bg-dark border border-primary'>
-            Jira
-          </span>
-          <span className='badge p-2 text-bg-dark border border-primary'>
-            Testrail
-          </span>
+          <span className='badge bg-dark border border-primary'>Jest</span>
+          <span className='badge bg-dark border border-primary'>Pytest</span>
+          <span className='badge bg-dark border border-primary'>Puppeteer</span>
+          <span className='badge bg-dark border border-primary'>Postman</span>
+          <span className='badge bg-dark border border-primary'>Jira</span>
+          <span className='badge bg-dark border border-primary'>Testrail</span>
         </div>
       </div>
-      <div className='container mt-3 m-0 p-0 fs-5 text'>
-        <div className='mt-2 d-block fs-5 text fw-semibold text-decoration-underline'>
-          Certificates
+
+      <div className='container mt-3 fs-4 text'>
+        <div className='text-start mb-0 pt-2 border-start ps-3 p-1 border-5 border-primary '>
+          <h2 className='fs-5 fw-semibold text-decoration-underline'>
+            Certificates
+          </h2>
         </div>
-        <div className='m-0 p-0 d-flex gap-2 py-4'>
-          <span className='badge p-2 text-bg-dark border border-primary'>
+        <div className='d-flex flex-wrap gap-2 py-4'>
+          <span className='badge bg-dark border border-primary'>
             ISTQB® Foundation Level
           </span>
-          <span className='badge p-2 text-bg-dark border border-primary'>
+          <span className='badge bg-dark border border-primary'>
             PSM I, Scrum.org
           </span>
-          <span className='badge p-2 text-bg-dark border border-primary'>
+          <span className='badge bg-dark border border-primary'>
             Postman API Expert
           </span>
-          <span className='badge p-2 text-bg-dark border border-primary'>
+          <span className='badge bg-dark border border-primary'>
             Cypress End-to-End Testing
           </span>
         </div>
