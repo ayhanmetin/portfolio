f9d76b7383acad6433c75ad9ad96b1e24cd4244d
Ayhan Metin <108268012+ayhanmetin@users.noreply.github.com>
2024-01-01 21:26:48 +0100

    Update Portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -22,15 +22,15 @@ function Portfolio() {
                     href={project.liveDemoUrl}
                     target='_blank'
                     rel='noopener noreferrer'
-                    className='link-light link-offset-2 link-underline-opacity-0'
+                    className='link-offset-2 link-underline-opacity-0'
                   >
                     <img
                       src={project.imageUrl}
-                      className='imageLoad img-fluid border border-primary-subtle border-5'
+                      className='imageLoad img-fluid border border-primary border-5'
                       alt={project.name}
                     />
                   </a>
-                  <div className='bg-primary-subtle position-absolute bottom-0 start-0'>
+                  <div className='imageLoad bg-primary position-absolute bottom-0 start-0'>
                     <div
                       className='text-white'
                       style={{
@@ -40,14 +40,14 @@ function Portfolio() {
                       }}
                     >
                       <h3
-                        className='fs-6 m-2 text fw-light '
+                        className='fs-6 p-0 text '
                         style={{ marginBottom: '2px' }}
                       >
                         <a
                           href={project.liveDemoUrl}
                           target='_blank'
                           rel='noopener noreferrer'
-                          className='link-light link-offset-2 link-underline-opacity-0'
+                          className='link-light p-1 link-offset-2 link-underline-opacity-0'
                         >
                           {project.name}
                         </a>
