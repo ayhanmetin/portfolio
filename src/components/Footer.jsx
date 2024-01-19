0dc296a2102ef257a004a69f7cf323c82fe51df9
Ayhan Metin <108268012+ayhanmetin@users.noreply.github.com>
2024-01-20 00:16:27 +0100

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,41 +1,41 @@
-import React from "react";
+import React from 'react';
 
 export default function Footer() {
   return (
     <>
-      <footer className="p-0 m-0">
-        <ul className="nav justify-content-center border-top pb-0 mb-0">
-          <li className="nav-item">
+      <footer className='p-0 m-0'>
+        <ul className='nav justify-content-center border-top pb-0 mb-0'>
+          <li className='nav-item'>
             <a
-              href="mailto:ayhanmetinde@gmail.com"
-              className="nav-link px-2 text-body-secondary"
+              href='mailto:ayhanmetinde@gmail.com'
+              className='nav-link px-2 text-body-secondary'
             >
               E-mail
             </a>
           </li>
-          <li className="nav-item">
+          <li className='nav-item'>
             <a
-              target="_blank"
-              href="https://www.linkedin.com/in/ayhanmet/"
-              className="nav-link px-2 text-body-secondary"
+              target='_blank'
+              href='https://www.linkedin.com/in/ayhanmet/'
+              className='nav-link px-2 text-body-secondary'
             >
               Linkedin
             </a>
           </li>
-          <li className="nav-item">
+          <li className='nav-item'>
             <a
-              target="_blank"
-              href="https://github.com/ayhanmetin"
-              className="nav-link px-2 text-body-secondary"
+              target='_blank'
+              href='https://github.com/ayhanmetin'
+              className='nav-link px-2 text-body-secondary'
             >
               Github
             </a>
           </li>
-          <li className="nav-item">
+          <li className='nav-item'>
             <a
-              target="_blank"
-              href="https://instagram.com/ayhanmett"
-              className="nav-link px-2 text-body-secondary"
+              target='_blank'
+              href='https://instagram.com/ayhanmett'
+              className='nav-link px-2 text-body-secondary'
             >
               Instagram
             </a>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,41 +1,41 @@
-import React from "react";
-import writings from "../data/writings";
-import githubTesting from "../data/githubTesting";
-import projects from "../data/projects";
+import React from 'react';
+import writings from '../data/writings';
+import githubTesting from '../data/githubTesting';
+import projects from '../data/projects';
 
 export default function Home() {
   return (
     <>
-      <div className="container mb-5">
-        <div className="row justify-content-center">
-          <div className="col-lg-8">
-            <h4 className="pb-2">Welcome!</h4>
+      <div className='container mb-5'>
+        <div className='row justify-content-center'>
+          <div className='col-lg-8'>
+            <h4 className='pb-2 fw-bolder'>Welcome!</h4>
             <p>
               I am Ayhan Metin, passionate about Web Applications and AI. I have
               mastered test automation using Selenium WebDriver and Cypress, and
               agile practices. Feel free to connect with me on &nbsp;
               <a
-                target="_blank"
-                href="https://www.linkedin.com/in/ayhanmet/"
-                className="fs-7 text fw-medium link-body-emphasis link-offset-2 link-underline-opacity-25 link-underline-opacity-75-hover"
+                target='_blank'
+                href='https://www.linkedin.com/in/ayhanmet/'
+                className='fs-7 text fw-medium link-body-emphasis link-offset-2 link-underline-opacity-25 link-underline-opacity-75-hover'
               >
                 LinkedIn
               </a>
               &nbsp; for further discussion or opportunities.
             </p>
 
-            <h5 className="my-4">Experience and Expertise</h5>
+            <h5 className='my-4 fw-bolder'>Experience and Expertise</h5>
             <article>
               <p>
                 I have years of experience in making Web UIs for e-commerce work
                 better, doing manual and A/B tests to make sure they work as
                 they should, are reliable, and easy to use. I've worked on many
                 freelance projects and am confident in improving automation for
-                your projects. Recently, I developed a website for{" "}
+                your projects. Recently, I developed a website for{' '}
                 <a
-                  target="_blank"
-                  href="https://www.birsasigorta.com.tr"
-                  className="fs-7 text fw-medium link-body-emphasis link-offset-2 link-underline-opacity-25 link-underline-opacity-75-hover"
+                  target='_blank'
+                  href='https://www.birsasigorta.com.tr'
+                  className='fs-7 text fw-medium link-body-emphasis link-offset-2 link-underline-opacity-25 link-underline-opacity-75-hover'
                 >
                   Birsa Insurance.
                 </a>
@@ -46,7 +46,7 @@ export default function Home() {
                 knowledge about tools like Testrail, Appium, Playwright,
                 RestAssured and more.
               </p>
-              <h5 className="my-4">Automation Practice</h5>
+              <h5 className='my-4 fw-bolder'>Automation Practice</h5>
               <p>
                 Explore my collection of practical webpages and exercises,
                 including detailed Cypress login page tutorials and GitHub
@@ -54,17 +54,17 @@ export default function Home() {
                 showcasing a range of hands-on exercises designed to enhance
                 your understanding and skills.
               </p>
-              <div className="container p-0 mt-4 mb-2 fs-4 text">
+              <div className='container p-0 mt-4 mb-2 fs-4 text'>
                 {githubTesting.map((test, index) => (
                   <div key={index}>
                     <ul>
                       <figure>
-                        <blockquote className="blockquote fs-6 text">
+                        <blockquote className='blockquote fs-6 text'>
                           <p>
                             <a
-                              className="link-body-emphasis link-offset-2 
-                            link-underline-opacity-25 link-underline-opacity-75-hover"
-                              target="_blank"
+                              className='link-body-emphasis link-offset-2 
+                            link-underline-opacity-25 link-underline-opacity-75-hover'
+                              target='_blank'
                               href={test.githubLink}
                             >
                               <li>{test.title}</li>
@@ -76,22 +76,22 @@ export default function Home() {
                   </div>
                 ))}
               </div>
-              <h5 className="my-4">Articles</h5>
+              <h5 className='my-4 fw-bolder'>Articles</h5>
               <p>
                 Dive into my articles where I discuss and delve deeply into
                 various aspects of test automation, development, and the latest
                 technological updates.
               </p>
-              <div className="container p-0 mt-4 mb-2 fs-4 text">
+              <div className='container p-0 mt-4 mb-2 fs-4 text'>
                 {writings.map((writing, index) => (
                   <div key={index}>
                     <figure>
-                      <blockquote className="blockquote fs-6 text">
+                      <blockquote className='blockquote fs-6 text'>
                         <p>
                           <a
-                            className="link-body-emphasis link-offset-2 
-                            link-underline-opacity-25 link-underline-opacity-75-hover"
-                            target="_blank"
+                            className='link-body-emphasis link-offset-2 
+                            link-underline-opacity-25 link-underline-opacity-75-hover'
+                            target='_blank'
                             href={writing.link}
                           >
                             {writing.title}
@@ -103,23 +103,23 @@ export default function Home() {
                 ))}
               </div>
               <div></div>
-              <h5 className="mt-4">Beyond Test Automation</h5>
+              <h5 className='mt-4 fw-bolder'>Beyond Test Automation</h5>
               <p>
                 Here are some of the projects I am proud of in Frontend
                 Development, featuring React, JavaScript, Bootstrap, TailWind,
                 and OpenAI API.
               </p>
-              <div className="container p-0 mt-4 mb-2 fs-4 text">
+              <div className='container p-0 mt-4 mb-5 fs-4 text'>
                 {projects.map((project, index) => (
                   <div key={index}>
                     <ul>
                       <figure>
-                        <blockquote className="blockquote fs-6 text">
+                        <blockquote className='blockquote fs-6 text'>
                           <p>
                             <a
-                              className="link-body-emphasis link-offset-2 
-                            link-underline-opacity-25 link-underline-opacity-75-hover"
-                              target="_blank"
+                              className='link-body-emphasis link-offset-2 
+                            link-underline-opacity-25 link-underline-opacity-75-hover'
+                              target='_blank'
                               href={project.liveDemoUrl}
                             >
                               <li>{project.name}</li>
@@ -131,7 +131,7 @@ export default function Home() {
                   </div>
                 ))}
               </div>
-              <h5>Looking Ahead</h5>
+              <h5 className='fw-bolder'>Looking Ahead</h5>
               <p>
                 Feel free to contact me to share insights on the latest
                 technologies and more.
