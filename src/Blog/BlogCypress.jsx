5b7072212684b8b0d383e22e164467cfff824ced
Ayhan Metin <108268012+ayhanmetin@users.noreply.github.com>
2024-01-24 01:20:24 +0100

    update

---
+++ src/Blog/BlogCypress.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+export default function BlogCypress() {
+  return (
+    <>
+      <h3 className='pb-4 mb-4 fst-italic border-bottom'>From the Firehose</h3>
+
+      <article className='blog-post'>
+        <h2 className='display-5 link-body-emphasis mb-1'>Sample blog post</h2>
+        <p className='blog-post-meta'>
+          January 1, 2021 by <a href='#'>Mark</a>
+        </p>
+
+        <p>
+          This blog post shows a few different types of content that’s supported
+          and styled with Bootstrap. Basic typography, lists, tables, images,
+          code, and more are all supported as expected.
+        </p>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum delectus accusamus fuga veritatis, in dicta facere saepe sapiente eum voluptate deserunt soluta hic laboriosam rem repellat optio? Ex, perspiciatis expedita.
+        Amet, voluptatibus, quis quae, illo in modi enim voluptas quod deleniti cumque eaque iusto perspiciatis odit nostrum porro dicta. Quas ducimus officiis sunt quo accusantium fugiat! Recusandae veritatis deserunt velit.
+        Repellendus dolores, ducimus rerum magnam, non nemo fugit dolorum labore itaque animi voluptatibus? Beatae corporis velit sunt quam debitis voluptates, provident sit, distinctio similique fuga ut corrupti nisi, quaerat cupiditate!
+        Laborum unde, reiciendis modi iure, tenetur sequi quo earum quae cum esse cumque optio praesentium! Totam nesciunt facere cupiditate reprehenderit beatae deleniti? Facere, ut sit dolore maiores adipisci exercitationem consequuntur?
+        At ipsum saepe officiis omnis, voluptas corporis molestias nihil illo quo maiores facere velit possimus debitis hic, veniam tempora corrupti molestiae rem? Sit deserunt, cum ea tempore expedita voluptatibus consectetur.
+        Reiciendis laudantium aliquam harum molestias a? Reiciendis quod delectus voluptatum ullam modi mollitia dicta tempora nostrum sint. Fugiat accusantium aperiam obcaecati praesentium vero eos, laboriosam blanditiis, non sit ipsa officia.
+        Perspiciatis ad, tempora porro non, dolorum quam ea ipsam asperiores similique facilis suscipit. Earum vitae eos obcaecati nihil atque corrupti facere, repellendus dolores omnis. Placeat totam beatae sit eligendi facere!
+        Consequatur voluptates doloremque temporibus voluptatum neque? Dicta tenetur nesciunt maxime molestiae ex distinctio numquam, ratione doloremque reiciendis veniam a, rerum mollitia fugit eligendi est cupiditate voluptatum impedit vitae, labore aperiam!
+        Omnis, asperiores totam deserunt voluptate, eligendi delectus vitae similique inventore expedita quos esse nulla hic assumenda quidem corrupti? Officiis iure quo cum, eligendi possimus voluptatum qui velit quibusdam earum eius?
+        Ab tempore cupiditate fugiat architecto. Autem in cum nostrum soluta beatae ad dolorem eaque, odio laborum incidunt. Consequuntur deserunt dolorum possimus, necessitatibus atque reiciendis dicta praesentium fugit laborum dolores tempore!
+        Nihil at temporibus adipisci quam maiores quo esse voluptatem consectetur iure eligendi laudantium sit numquam modi, est molestiae rem ea, dolores ullam ducimus tempore. Maxime nisi at aperiam quia officia!
+        Quia explicabo illo harum quos quae voluptatum corrupti blanditiis fugit, vel eos maiores est quidem, sunt saepe quasi dignissimos fugiat quaerat error, magnam aliquam atque? Consectetur repellendus perspiciatis ad accusamus?
+        Incidunt esse eum nesciunt assumenda quaerat officia dolorum illum iusto officiis voluptatem tempore veniam corporis adipisci tenetur neque debitis molestiae reiciendis, nostrum autem quibusdam facere consequuntur quae quo. Neque, quos!
+        Reiciendis eos voluptates mollitia, omnis officia quibusdam id. Distinctio amet quos magnam est hic, doloribus perferendis optio impedit, dolore odit eum totam voluptatibus omnis, mollitia dolor ut quidem? Id, cum.
+        Sapiente consectetur saepe provident. Dolores ab deleniti voluptatibus aut amet eum dolorum quis quaerat ad repellat, autem nesciunt dicta deserunt molestias illo odit hic reprehenderit? Magnam, hic dolorum! Ab, veniam.</p>
+      </article>
+    </>
+  );
+}
--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,44 +1,161 @@
-import React from "react";
+import React from 'react';
+import BlogCypress from '../Blog/BlogCypress';
 
 export default function Blog() {
   return (
-    <div className="container mb-5">
-      <div className="row justify-content-center">
-        <div className="col-lg-8">
-        <div>Coming soon...</div>
-          {/* <h2 className="display-5 link-body-emphasis mb-1">
-            Sample blog post
-          </h2>
-          <p className="blog-post-meta">
-            January 1, 2021 by <a href="#">Mark</a>
-          </p>
-          <p>
-            This blog post shows a few different types of content that’s
-            supported and styled with Bootstrap. Basic typography, lists,
-            tables, images, code, and more are all supported as expected.
-          </p>
-          <hr />
-          <p>
-            This is some additional paragraph placeholder content. It has been
-            written to fill the available space and show how a longer snippet of
-            text affects the surrounding content. We'll repeat it often to keep
-            the demonstration flowing, so be on the lookout for this exact same
-            string of text.
-          </p>
-          <h2>Blockquotes</h2>
-          <p>This is an example blockquote in action:</p>
-          <blockquote className="blockquote">
-            <p>Quoted text goes here.</p>
-          </blockquote>
-          <p>
-            This is some additional paragraph placeholder content. It has been
-            written to fill the available space and show how a longer snippet of
-            text affects the surrounding content. We'll repeat it often to keep
-            the demonstration flowing, so be on the lookout for this exact same
-            string of text.
-          </p> */}
-        </div>
-      </div>
-    </div>
+    <div>Coming soon..</div>
+    // <main className='container'>
+    //   <div className='p-4 p-md-5 mb-4 rounded text-body-emphasis bg-body-secondary'>
+    //     <div className='col-lg-6 px-0'>
+    //       <h1 className='display-4 fst-italic'>
+    //         Title of a longer featured blog post
+    //       </h1>
+    //       <p className='lead my-3'>
+    //         Multiple lines of text that form the lede, informing new readers
+    //         quickly and efficiently about what’s most interesting in this post’s
+    //         contents.
+    //       </p>
+    //       <p className='lead mb-0'>
+    //         <a href='#' className='text-body-emphasis fw-bold'>
+    //           Continue reading...
+    //         </a>
+    //       </p>
+    //     </div>
+    //   </div>
+
+    //   <div className='row g-5'>
+    //     <div className='col-md-8'>
+    //       <BlogCypress />
+    //       <BlogCypress />
+    //       <BlogCypress />
+    //     </div>
+
+    //     <div className='col-md-4'>
+    //       <div className='position-sticky' style={{ top: '2rem' }}>
+    //         <div className='p-4 mb-3 bg-body-tertiary rounded'></div>
+    //         <div>
+    //           <h4 className='fst-italic'>Recent posts</h4>
+    //           <ul className='list-unstyled'>
+    //             <li>
+    //               <a
+    //                 className='d-flex flex-column flex-lg-row gap-3 align-items-start align-items-lg-center py-3 link-body-emphasis text-decoration-none border-top'
+    //                 href='#'
+    //               >
+    //                 <svg
+    //                   className='bd-placeholder-img'
+    //                   width='100%'
+    //                   height='96'
+    //                   xmlns='http://www.w3.org/2000/svg'
+    //                   aria-hidden='true'
+    //                   preserveAspectRatio='xMidYMid slice'
+    //                   focusable='false'
+    //                 >
+    //                   <rect width='100%' height='100%' fill='#777'></rect>
+    //                 </svg>
+    //                 <div className='col-lg-8'>
+    //                   <h6 className='mb-0'>Example blog post title</h6>
+    //                   <small className='text-body-secondary'>
+    //                     January 15, 2023
+    //                   </small>
+    //                 </div>
+    //               </a>
+    //             </li>
+    //             <li>
+    //               <a
+    //                 className='d-flex flex-column flex-lg-row gap-3 align-items-start align-items-lg-center py-3 link-body-emphasis text-decoration-none border-top'
+    //                 href='#'
+    //               >
+    //                 <svg
+    //                   className='bd-placeholder-img'
+    //                   width='100%'
+    //                   height='96'
+    //                   xmlns='http://www.w3.org/2000/svg'
+    //                   aria-hidden='true'
+    //                   preserveAspectRatio='xMidYMid slice'
+    //                   focusable='false'
+    //                 >
+    //                   <rect width='100%' height='100%' fill='#777'></rect>
+    //                 </svg>
+    //                 <div className='col-lg-8'>
+    //                   <h6 className='mb-0'>This is another blog post title</h6>
+    //                   <small className='text-body-secondary'>
+    //                     January 14, 2023
+    //                   </small>
+    //                 </div>
+    //               </a>
+    //             </li>
+    //             <li>
+    //               <a
+    //                 className='d-flex flex-column flex-lg-row gap-3 align-items-start align-items-lg-center py-3 link-body-emphasis text-decoration-none border-top'
+    //                 href='#'
+    //               >
+    //                 <svg
+    //                   className='bd-placeholder-img'
+    //                   width='100%'
+    //                   height='96'
+    //                   xmlns='http://www.w3.org/2000/svg'
+    //                   aria-hidden='true'
+    //                   preserveAspectRatio='xMidYMid slice'
+    //                   focusable='false'
+    //                 >
+    //                   <rect width='100%' height='100%' fill='#777'></rect>
+    //                 </svg>
+    //                 <div className='col-lg-8'>
+    //                   <h6 className='mb-0'>
+    //                     Longer blog post title: This one has multiple lines!
+    //                   </h6>
+    //                   <small className='text-body-secondary'>
+    //                     January 13, 2023
+    //                   </small>
+    //                 </div>
+    //               </a>
+    //             </li>
+    //           </ul>
+    //         </div>
+    //         <div className='p-4'>
+    //           <h4 className='fst-italic'>Archives</h4>
+    //           <ol className='list-unstyled mb-0'>
+    //             <li>
+    //               <a href='#'>March 2021</a>
+    //             </li>
+    //             <li>
+    //               <a href='#'>February 2021</a>
+    //             </li>
+    //             <li>
+    //               <a href='#'>January 2021</a>
+    //             </li>
+    //             <li>
+    //               <a href='#'>December 2020</a>
+    //             </li>
+    //             <li>
+    //               <a href='#'>November 2020</a>
+    //             </li>
+    //             <li>
+    //               <a href='#'>October 2020</a>
+    //             </li>
+    //             <li>
+    //               <a href='#'>September 2020</a>
+    //             </li>
+    //             <li>
+    //               <a href='#'>August 2020</a>
+    //             </li>
+    //             <li>
+    //               <a href='#'>July 2020</a>
+    //             </li>
+    //             <li>
+    //               <a href='#'>June 2020</a>
+    //             </li>
+    //             <li>
+    //               <a href='#'>May 2020</a>
+    //             </li>
+    //             <li>
+    //               <a href='#'>April 2020</a>
+    //             </li>
+    //           </ol>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </main>
   );
 }
